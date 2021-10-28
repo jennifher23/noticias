b09cd555a5f43b7064f82f09faa631785cb2fe94
Jenny <86363759+jennifher23@users.noreply.github.com>
2021-10-28 09:16:57 -0500

    Add files via upload

---
+++ noticias/img/INICIO_files/js-d2793-01526.js
@@ -0,0 +1,6 @@
+
+
+/*===============================
+/templates/ts_dailytimes/js/script.js
+================================================================================*/;
+jQuery(function($){"use strict";$(window).on('scroll',function(){if($(window).scrollTop()>100){$('.t3-mainnav').addClass('navbar-fixed');}else{$('.t3-mainnav').removeClass('navbar-fixed');}});});
